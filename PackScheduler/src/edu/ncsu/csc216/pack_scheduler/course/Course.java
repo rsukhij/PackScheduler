efd525641cf61ff86712cd1bf6b87b87f54029ee
Raymond Dong <rdong3@ncsu.edu>
2020-07-21 19:42:42 -0400

    Rewind since last push broke things

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/Course.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/Course.java
@@ -52,7 +52,7 @@ public class Course extends Activity implements Comparable<Course> {
 		setName(name);
 		setSection(section);
 		setCredits(credits);
-		instructorId = null;
+		setInstructorId(instructorId);
 		roll = new CourseRoll(this, enrollmentCap);
 	}
 
@@ -161,6 +161,9 @@ public class Course extends Activity implements Comparable<Course> {
 	 * @param instructorId the instructorId to set
 	 */
 	public void setInstructorId(String instructorId) {
+		if (instructorId == null || instructorId.equals("")) {
+			throw new IllegalArgumentException("Invalid instructor unity id");
+		}
 		this.instructorId = instructorId;
 	}
 
